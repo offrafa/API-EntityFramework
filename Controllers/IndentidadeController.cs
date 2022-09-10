f251509c836e8bffc13cbaaf83a93c2740cfc516
Rafael <rafaeltavares462@gmail.com>
2022-09-10 19:38:24 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/IndentidadeController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndentidadeController : ControllerBase
+    {
+        private readonly DataContext _Context;
+        public IndentidadeController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<IndentidadeModel> Get()
+        {
+            return _Context.Indentidades;
+        }
+
+        [HttpGet("{id}")]
+        public IndentidadeModel GetById(int id)
+        {
+            return _Context.Indentidades.FirstOrDefault(IndentidadeModel => IndentidadeModel.Id == id);
+        }
+    }
+}
---
+++ Migrations/20220910222347_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Indentidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SobreNome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaisDeOrigem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeNacimento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CodigoDoPais = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Indentidades", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Indentidades");
+        }
+    }
+}
